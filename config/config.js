29681307e5971b19b343aac33df3fa4e52985612
Marc Wright <marc.wright@gmail.com>
2019-12-06 10:10:15 -0500

    first commit

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  jwtSecret: process.env.JWTSECRET,
+  jwtSession: {
+    session: false
+  }
+}
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+const jwt = require('jwt-simple')
+const passport = require('../config/passport')
+const config = require('../config/config')
+const User = require('../models/User')
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  User.find()
+    .then(users => {
+      res.json({ users })
+    })
+});
+
+/* SIGNUP a user. */
+router.post('/signup', (req, res) => {
+  if (req.body.email && req.body.password) {
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          User.create(newUser)
+            .then(user => {
+              res.json({ user })
+            })
+        } else {
+          res.sendStatus(401)
+        }
+      })
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+router.post('/login', (req, res) => {
+  if (req.body.email && req.body.password) {
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        if (user.password === req.body.password) {
+          const payload = {
+            id: user.id
+          }
+          const token = jwt.encode(payload, config.jwtSecret)
+          res.json({ token, user })
+        } else {
+          res.sendStatus(401)
+        }
+      } else {
+        res.sendStatus(401)
+      }
+    })
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+router.post('/', (req, res) => {
+  console.log(req.headers)
+  var decoded = jwt.decode(req.headers.authorization, config.jwtSecret);
+  console.log(decoded); //=> { foo: 'bar' }
+  User.findById(decoded.id)
+    .then(user => {
+      res.json({ user })
+    })
+})
+
+module.exports = router;
